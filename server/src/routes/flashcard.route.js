114897af17caa14729b08d6c0d9ef32a0635f428
Bitan Das <biggyansagar@gmail.com>
2025-02-16 23:58:04 +0530

    updated singleplayer

--- server/src/routes/flashcard.route.js
+++ server/src/routes/flashcard.route.js
@@ -8,7 +8,7 @@ import {
   flashcard_view,
   flashcard_view_all,
   flashcard_view_topicname,
-  flashcard_view_of_collection
+  flashcard_view_of_collection, flashcard_show,
 } from "../controllers/flashcard.controller.js";
 
 import authToken from "../middlewares/authToken.js";
@@ -18,6 +18,7 @@ router.get("/view", authToken, flashcard_view); //Flashcard Collection view
 router.get("/view-all", authToken, flashcard_view_all); //Flashcard Collection view
 router.get("/view/:topic", authToken, flashcard_view_topicname); //Flashcard view of a topic of a particular collection
 router.get("/view/flashcard/:flashcardCollectionId" , authToken , flashcard_view_of_collection);//get all flashcards for an flashcardCollectionId
+router.get("/show", authToken, flashcard_show); //Flashcard Collection view
 router.put("/edit/:flashcardId", authToken, flashcard_edit);   // Edit flashcard
 router.delete("/delete/:flashcardId", authToken, flashcard_delete); // Delete flashcard
 
